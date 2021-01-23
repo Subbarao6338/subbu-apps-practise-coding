bf64c4842009a945b0fb218ae7e6efdd5a4a71e6
SUBBU6338 <subbu6338@outlook.in>
2021-01-23 15:04:10 +0530

    patterns

--- p1/patterns.py
+++ p1/patterns.py
@@ -186,33 +186,11 @@ for i in range(0,n):
         print(x,end=" ")
     print("\r")
 #21 pascal triangle
-r=1
-if(k>n-k):
-    k=n-k
-for i in range(0,k):
-    r=r*(n-i)
-    r=r//(i+1)
-
-for i in range(0,n):
-    for j in range(0,i+1):
-        print(r," ",end=" ")
-    print()
-
-# Print Pascal's Triangle in Java
 from math import factorial
-
-# input n
-n = 5
 for i in range(n):
-	for j in range(n-i+1):
-
-		# for left spacing
-		print(end=" ")
-
-	for j in range(i+1):
-
-		# nCr = n!/((n-r)!*r!)
-		print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
-
-	# for new line
-	print()
+    for j in range(n-i+1):
+        print(end=" ")
+    for j in range(i+1):
+        # nCr = n!/((n-r)!*r!)
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+    print()
