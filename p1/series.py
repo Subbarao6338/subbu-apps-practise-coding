2cceb0ac8605855e863be068061ddec2e1cb502c
SUBBU6338 <subbu6338@outlook.in>
2021-01-27 16:38:40 +0530

    patterns

--- p1/series.py
+++ p1/series.py
@@ -1,15 +1,35 @@
 # fibonacci
+#n th term
 n=int(input("n:"))
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return b
+    else:
+        for i in range(1, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+print(fibonacci(n))
+#series
 a=0
 b=1
-print(a,b)
-for j in range(0,n):
-    c=a+b
-    print(c,end=" ")
-    a=b
-    b=c
-'''
-# 1.check
+if n<=0:
+    print(a)
+else:
+    print(a,b,end=" ")
+    for i in range(0,n):
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
+#check
 import math
 def perfectsquare(x):
     s=int(math.sqrt(x))
@@ -24,7 +44,7 @@ def fib(i):
         print(i,"is not fib num")
 n=int(input("enter number:"))
 print(fib(n))
-# 2.range
+#range
 l=int(input("lower range:"))
 u=int(input("upper range:"))
 for f in range(l,u+1):
